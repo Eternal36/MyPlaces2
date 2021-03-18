c7cdb2e841f362c1bbd8dd3aa52aef082169535c
Eternal36 <33805342+Eternal36@users.noreply.github.com>
2021-03-18 16:23:02 +0300

    Custom Cell

---
+++ MyPlaces2/CustomTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces2
+//
+//  Created by Дмитрий Бельков on 18.03.2021.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+  
+    @IBOutlet var imageOfPlace: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    
+    @IBOutlet var typeLabel: UILabel!
+}
