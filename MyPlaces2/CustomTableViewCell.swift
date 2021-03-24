30501b3d260cd28e224a7bf5d32599216481290c
Eternal36 <33805342+Eternal36@users.noreply.github.com>
2021-03-24 14:48:26 +0300

    sorting

--- MyPlaces2/CustomTableViewCell.swift
+++ MyPlaces2/CustomTableViewCell.swift
@@ -11,8 +11,8 @@ class CustomTableViewCell: UITableViewCell {
 
   
     @IBOutlet var imageOfPlace: UIImageView!
+    
     @IBOutlet var nameLabel: UILabel!
     @IBOutlet var locationLabel: UILabel!
-    
     @IBOutlet var typeLabel: UILabel!
 }
